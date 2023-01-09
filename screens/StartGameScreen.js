cf3b3d9af605ed223a9e5b5c520e3be276b2750f
InSeong-So <goflvhxj2547@gmail.com>
2023-01-09 23:00:16 +0900

    Design: iOS, android button pressed 스타일 적용

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,12 @@ import PrimaryButton from '../components/PrimaryButton';
 export default function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -17,12 +22,23 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: '#72063c',
+    backgroundColor: '#4e0329',
     borderRadius: 8,
     elevation: 8,
     shadowColor: 'black',
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
