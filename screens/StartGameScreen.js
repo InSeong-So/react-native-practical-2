a3acb711d3bf202297dc53015b9789225498e0eb
InSeong-So <goflvhxj2547@gmail.com>
2023-01-10 15:38:14 +0900

    Feat: 버튼 스타일 개선

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -10,21 +10,30 @@ export default function StartGameScreen() {
         maxLength={2}
         keyboardType="number-pad"
         autoCapitalize="none"
+        autoCorrect={false}
       />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
     backgroundColor: '#4e0329',
     borderRadius: 8,
-    elevation: 8,
+    elevation: 4,
     shadowColor: 'black',
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
@@ -33,12 +42,18 @@ const styles = StyleSheet.create({
   numberInput: {
     height: 50,
     width: 50,
+    fontSize: 32,
     borderBottomColor: '#ddb52f',
     borderBottomWidth: 2,
     color: '#ddb52f',
     marginVertical: 8,
-    fontSize: 32,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
