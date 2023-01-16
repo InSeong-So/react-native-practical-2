bb4da6cda99b1804cbb7a945fef08685438af772
InSeong-So <goflvhxj2547@gmail.com>
2023-01-16 15:22:20 +0900

    Feat: 추측 가능한 수를 생성하는 함수와 컴포넌트를 작성

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import Title from '../components/Title';
+import NumberContainer from '../components/game/NumberContainer';
+import Title from '../components/ui/Title';
 import Colors from '../constants/colors';
 
-export default function GameScreen() {
+export default function GameScreen({ userNumber }) {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
   return (
     <View style={styles.screen}>
       <Title>업 & 다운 숫자 게임</Title>
-      {/* <Text>추측하기</Text> */}
+      <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Game Screen!</Text>
         {/* <Text>+ -</Text> */}
@@ -31,3 +36,17 @@ const styles = StyleSheet.create({
     padding: 12,
   },
 });
+
+const generateRandomBetween = (min, max, exclude) => {
+  /**
+   * Math.random은 숫자 하나를 배제한 채 0과 1 사이의 난수를 생성
+   * 여기에 최댓값과 최솟값의 차이를 곱해서 0과 1 사이의 난수를 최댓값과 최솟값 사이 난수로 전환하는 것
+   * Math.floor는 결과값보다 크지 않은 최대 정수를 반환한다. 위의 식은 소수 자리에 있는 십진수가 출력되므로
+   * 우리가 원하는 정수를 Math.floor로 만들어낸다.
+   * 소수 자리를 정리했으므로 하한값을 다시 더해준다. 0을 최솟값으로 얻지 않도록.
+   * 이렇게 설정하지 않으면 Math.random의 결과값이 0일 때 최솟값도 0이 될 것이다.
+   * 그러나 하한값을 정하면 여기에서 정의한 최솟값이 하한값이 될 것이다.
+   */
+  const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+  return randomNumber === exclude ? generateRandomBetween(min, max, exclude) : randomNumber;
+};
