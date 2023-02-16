545e4b417311979d5a73e08363359a91aba173e7
InSeong-So <goflvhxj2547@gmail.com>
2023-02-16 17:04:09 +0900

    Feat: 전경 추가하기

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,35 @@
-import { Text } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import Title from '../components/ui/Title';
+import Colors from '../constants/colors';
 
 export default function GameOverScreen() {
-  <Text>Game is Over!</Text>;
+  <View style={styles.rootContainer}>
+    <Title>게임 종료!</Title>
+    <View style={styles.imageContainer}>
+      <Image style={styles.image} source={require('../assets/images/success.png')} />
+    </View>
+    <Text></Text>
+  </View>;
 }
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: 'hidden',
+    margin: 36,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
